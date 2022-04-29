1298688ddf20282740daa8fc885d0beb3f547e95
Valentin Voica <valentin_voiclla@live.com>
2022-04-29 16:07:18 +0300

    Rezolvare laborator 1

--- ExempluExercitii/Program.cs
+++ ExempluExercitii/Program.cs
@@ -9,8 +9,9 @@ namespace ExempluExercitii
             //  Cele doua forward slash "//" in fata unei linii de cod marcheaza linia ca fiind comentata iar instructiunile de pe linie nu vor fi rulate.
             // Acum va rula exercitiu 2
             // Pentru a rula exercitiul 1 vom sterge "//" din fata liniei 12, "//Ex1" si vom comenta linia 13 adaugand "//" in fata textului "Ex2"
-            //Ex1();
-            Ex2();
+
+            Ex1();
+            //Ex2();
             //Ex3();
             //Ex5();
             //Ex6();
@@ -18,39 +19,197 @@ namespace ExempluExercitii
             //Ex8();
             //Ex9();
             //Ex10();
+
             // Sa nu uitati sa salvati in mod frecvent (Ctrl+S) pentru a nu pierde munca
+            Console.ReadLine();
         }
         static  void Ex1()
         {
-            // aici vom pune rezolvarea pentru exercitiu 1
+            /*Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+            paralelipiped dreptunghic, va afisa volumul lui*/
+
+            Console.WriteLine("Introduceti lungimea");
+            int lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latimea");
+            int latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea");
+            int inaltime = int.Parse(Console.ReadLine());
+
+            int volumulParalelipipedului = lungime * latime * inaltime;
+
+            Console.WriteLine("Volumul paralelipipedului este " + volumulParalelipipedului);
+
         }
         static void Ex2()
         {
-            // aici vom pune rezolvarea pentru exercitiu 2
+            // Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura
+
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar");
+            int z = int.Parse(Console.ReadLine());
+
+            int mediaAritmetica = (x + y + z) / 3;
+
+            Console.WriteLine("Media aritmetica a numerelor este " + mediaAritmetica);
         }
         static void Ex3()
         {
-            // aici vom pune rezolvarea pentru exercitiu 3
+            // Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastaura
+            Console.WriteLine("Introduceti numarul ");
+            int numarul = int.Parse(Console.ReadLine());
+
+            int ultimaCifra = numarul % 10;
+
+            Console.WriteLine("ultima cifra a numarului este " + ultimaCifra);
         }
         static void Ex4()
         {
-            // aici vom pune rezolvarea pentru exercitiu 4
+            // Scrieti un program care va afisa semnul unui numar citit de la tastatura
+            //   • Daca numarul este pozitiv, va afisa “+”
+            //   • Daca numarul este negativ, va afisa “-”
+            //   • Daca numarul este 0, va afisa “0”
+
+            Console.WriteLine("Introduceti numarul ");
+            int numarul = int.Parse(Console.ReadLine());
+
+            if (numarul > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (numarul < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else 
+            {
+                Console.WriteLine("0");
+            }
+
         }
         static void Ex5()
         {
-            // aici vom pune rezolvarea pentru exercitiu 5
+            // Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x>y)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+            }
+            else 
+            {
+                Console.WriteLine(y);
+                Console.WriteLine(x);
+            }
         }
         static void Ex6()
         {
-            // aici vom pune rezolvarea pentru exercitiu 6
+            // Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine descrescatoare.
+
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar");
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numerele ordonate");
+            if (x > y)
+            {                
+                if (z > x)
+                {
+                    //zxy
+                    Console.WriteLine(z);
+                    Console.WriteLine(x);
+                    Console.WriteLine(y);
+                }else if (y > z)
+                {
+                    //xyz
+                    Console.WriteLine(x);
+                    Console.WriteLine(y);
+                    Console.WriteLine(z);
+                }
+                else
+                {
+                //xzy
+                    Console.WriteLine(x);
+                    Console.WriteLine(z);
+                    Console.WriteLine(y);
+                }            
+            }
+            else {
+                if (z > y)
+                {
+                    //zyx
+                    Console.WriteLine(z);
+                    Console.WriteLine(y);
+                    Console.WriteLine(x);
+                }
+                else if (x>z)
+                {
+                    //yxz
+                    Console.WriteLine(y);
+                    Console.WriteLine(x);
+                    Console.WriteLine(z);
+                }
+                else
+                {
+                    //yzx
+                    Console.WriteLine(y);
+                    Console.WriteLine(z);
+                    Console.WriteLine(x);
+                }
+            }
         }
         static void Ex7()
         {
-            // aici vom pune rezolvarea pentru exercitiu 7
+            // Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar
+
+            Console.WriteLine("Introduceti numarul ");
+            int numar = int.Parse(Console.ReadLine());
+
+            if (numar % 2 == 0)
+            {
+                Console.WriteLine("par");
+
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
         }
         static void Ex8()
         {
-            // aici vom pune rezolvarea pentru exercitiu 8
+            // Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica daca x este divizibil cu y.
+
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x % y == 0)
+            {
+                Console.WriteLine("Divizibile");
+            }
+            else
+            {
+                Console.WriteLine("indivizibile");
+
+            }
         }
         static void Ex9()
         {
@@ -58,7 +217,13 @@ namespace ExempluExercitii
         }
         static void Ex10()
         {
-            // aici vom pune rezolvarea pentru exercitiu 10
+            //  Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la tastatura.
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            int sumaCifrelor = x / 10 + x % 10;
+
+            Console.WriteLine("suma cifrelor este " + sumaCifrelor);
         }
     }
 }
