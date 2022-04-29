1c102b727c34322af21acfca2b04476766d66f96
Valentin Voica <valentin_voiclla@live.com>
2022-04-29 11:14:38 +0300

    Add project files.

---
+++ ExempluExercitii/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ExempluExercitii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Cele doua forward slash "//" in fata unei linii de cod marcheaza linia ca fiind comentata iar instructiunile de pe linie nu vor fi rulate.
+            // Acum va rula exercitiu 2
+            // Pentru a rula exercitiul 1 vom sterge "//" din fata liniei 12, "//Ex1" si vom comenta linia 13 adaugand "//" in fata textului "Ex2"
+            //Ex1();
+            Ex2();
+            //Ex3();
+            //Ex5();
+            //Ex6();
+            //Ex7();
+            //Ex8();
+            //Ex9();
+            //Ex10();
+            // Sa nu uitati sa salvati in mod frecvent (Ctrl+S) pentru a nu pierde munca
+        }
+        static  void Ex1()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 1
+        }
+        static void Ex2()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 2
+        }
+        static void Ex3()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 3
+        }
+        static void Ex4()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 4
+        }
+        static void Ex5()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 5
+        }
+        static void Ex6()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 6
+        }
+        static void Ex7()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 7
+        }
+        static void Ex8()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 8
+        }
+        static void Ex9()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 9
+        }
+        static void Ex10()
+        {
+            // aici vom pune rezolvarea pentru exercitiu 10
+        }
+    }
+}
